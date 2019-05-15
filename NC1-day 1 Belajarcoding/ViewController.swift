e7ccd2a57680ce93b62234fac68737374ad1f4f9
louis <lrv1302@gmail.com>
2019-05-15 08:21:45 +0700

    gua emng ganteng

---
+++ NC1-day 1 Belajarcoding/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NC1-day 1 Belajarcoding
+//
+//  Created by Louis  Valen on 15/05/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("gua ganteng")
+    }
+
+
+}
+
